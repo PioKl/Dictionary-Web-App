5242d9cc7b5129fed47c56d25ad219e7959903a8
PioKl <kosiu342@gmail.com>
2024-06-25 21:07:33 +0200

    Add Link to main page

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,10 +1,14 @@
 import "../style/ErrorPage.scss";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
   return (
     <div className="errorPage">
       <span className="errorPage__emoji">😕</span>
       <h2 className="errorPage__heading">Wrong Page</h2>
+      <Link to="/" className="errorPage__return">
+        Go To Main Page
+      </Link>
     </div>
   );
 }

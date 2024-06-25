72a73a3b0c996522bb19b9b0b6044ae2d3ee928c
PioKl <kosiu342@gmail.com>
2024-06-25 20:56:56 +0200

    Add ErrorPage component

---
+++ src/components/ErrorPage.tsx
@@ -0,0 +1,10 @@
+import "../style/ErrorPage.scss";
+
+export default function ErrorPage() {
+  return (
+    <div className="errorPage">
+      <span className="errorPage__emoji">😕</span>
+      <h2 className="errorPage__heading">Wrong Page</h2>
+    </div>
+  );
+}
